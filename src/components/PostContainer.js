5014eb6202242ae1718ed81e89246ad75492f036
7204-OMER <Eyupbhr24@gmail.com>
2021-09-07 23:38:50 +0300

    add postcontainer.js

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import PostCard from "./PostCard";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import PostCard from "./PostCard";
+// import background from "../assets/background.jpg";
 // import loading from "../assets/loading.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
     margin: theme.spacing(1),
+    backgroundColor: "white",
   },
   blogContainer: {
     marginLeft: 170,
@@ -16,26 +20,22 @@ const useStyles = makeStyles((theme) => ({
   image: {
     marginLeft: "30%",
   },
-}));
 
+
+}));
 const PostContainer = () => {
+  const { currentBlogs } = useContext(AuthContext);
 
   // const { blogList, isLoading } = useFetch();
   const classes = useStyles();
 
   return (
     <div>
-      <h1 style={{ color: "purple" }}> DASHBOARD </h1>
+      <h1 style={{ color: "#F5835E" }}> DASHBOARD </h1>
       <div className={classes.blogContainer}>
-        {/* {isLoading ? (
-          <img className={classes.image} src={loading} alt="loading" />
-        ) : (
-          <>
-            {blogList?.map((item, index) => (
+            {currentBlogs?.map((item, index) => (
               <PostCard post={item} key={index} />
             ))}
-          </>
-        )} */}
       </div>
     </div>
   );
