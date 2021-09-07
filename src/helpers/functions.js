dbd43da87c3fbac8f0fb1d25e011a6e96c7e7943
7204-OMER <Eyupbhr24@gmail.com>
2021-09-07 23:39:10 +0300

    add func.js

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 const POSTS_URL = process.env.REACT_APP_MY_URL + "/postviewcreate/"
 
-export const getPosts = async() => {
-    const allPost = await axios.get(POSTS_URL)
-    return allPost
-}
+export const getPosts = async(setCurrentBlogs) => {
+    try {
+        const {data} = await axios.get(POSTS_URL)
+        setCurrentBlogs(data)
+    } catch (error) {
+        console.error(error.response.data.detail ? error.response.data.detail : error.response.statusText)
+    }
+    
+}   
