785e87b7d1e4ab348e620e5598e7087742f6c533
7204-OMER <Eyupbhr24@gmail.com>
2021-09-09 00:10:18 +0300

    add getPost func

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -7,7 +7,18 @@ export const getPosts = async(setCurrentBlogs) => {
         const {data} = await axios.get(POSTS_URL)
         setCurrentBlogs(data)
     } catch (error) {
-        console.error(error.response.data.detail ? error.response.data.detail : error.response.statusText)
+        console.error(error.response.data.detail? error.response.data.detail: error.response.statusText)
+    }
+    
+}   
+const POST_URL = process.env.REACT_APP_MY_URL + "/postdetail/"
+
+export const getPost = async(id, setPost) => {
+    try {
+        const {data} = await axios.get(POST_URL + id + "/")
+        setPost(data)
+    } catch (error) {
+        console.error(error.response.data.detail? error.response.data.detail: error.response.statusText)
     }
     
 }   
