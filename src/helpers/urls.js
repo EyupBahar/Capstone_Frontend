b48825dc4d6d0d61f535d55d644f82799663f229
7204-OMER <Eyupbhr24@gmail.com>
2021-09-19 22:56:35 +0300

    add logout urls

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,4 +1,5 @@
 export const POSTS_URL = process.env.REACT_APP_MY_URL + "postviewcreate/"
-export const POST_URL = process.env.REACT_APP_MY_URL + "postdetail/"
+export const POST_URL = process.env.REACT_APP_MY_URL + "profile/"
 export const LOGIN_URL = process.env.REACT_APP_MY_URL + "user/login/"
 export const REGISTER_URL = process.env.REACT_APP_MY_URL + "user/register/"
+export const LOGOUT_URL = process.env.REACT_APP_MY_URL + "user/logout/"
